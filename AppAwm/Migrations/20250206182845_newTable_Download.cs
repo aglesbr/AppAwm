bc274e3586b8effaaabc99b4e1c047436c318269
Agles Silva <agles@usa.com>
2025-02-06 21:54:57 -0300

    [HAS] criado a tela e tabela de gerenciamento de arquivos do sistema HDDOC

---
+++ AppAwm/Migrations/20250206182845_newTable_Download.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppAwm.Migrations
+{
+    /// <inheritdoc />
+    public partial class newTable_Download : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AWM_DOWNLOAD",
+                columns: table => new
+                {
+                    CD_DOWNLOAD = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NOME = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    DESCRICAO = table.Column<string>(type: "VARCHAR(150)", nullable: true),
+                    ANEXO = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    DT_CRIACAO = table.Column<DateTime>(type: "DATE", nullable: false),
+                    CD_USUARIO_CRIACAO = table.Column<string>(type: "VARCHAR(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AWM_DOWNLOAD", x => x.CD_DOWNLOAD);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AWM_DOWNLOAD");
+        }
+    }
+}
