c4ef2931ed641e2a27d8f1ffca8e4a3991eeebd9
Agles Silva <agles@usa.com>
2025-07-17 13:40:15 -0300

    [HAS] ajuste no usuario senha e rota do RabbitMQ V1

---
+++ AppAwm/Models/RabbitMqClient.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace AppAwm.Models
+{
+    public class RabbitMqClient
+    {
+        [JsonProperty("username")]
+        [JsonPropertyName("username")]
+        public string? UserName { get; set; }
+
+        [JsonProperty("password")]
+        [JsonPropertyName("password")]
+        public string? Password { get; set; }
+
+        [JsonProperty("host")]
+        [JsonPropertyName("host")]
+        public string? Host { get; set; }
+
+        [JsonProperty("port")]
+        [JsonPropertyName("port")]
+        public int Port { get; set; } = 5672;
+
+        [JsonProperty("canal")]
+        [JsonPropertyName("canal")]
+        public string? Canal { get; set; }
+
+        [JsonProperty("rota")]
+        [JsonPropertyName("rota")]
+        public string? Rota { get; set; }
+    }
+}
